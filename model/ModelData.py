a2995da939f9c46d4c39bbc9a41296e052761523
OscarVanL <7999692+OscarVanL@users.noreply.github.com>
2021-03-18 19:21:45 +0000

    Updated URI

--- model/ModelData.py
+++ model/ModelData.py
@@ -29,7 +29,7 @@ class ModelCovidImpacts:
             ModelGovtSchemes3.model_data(self.filename, data)
 
     def _create_schema(self, file):
-        file.write('@prefix : <http://oscarvl.synology.me/schema/covid-impacts/> .\n')
+        file.write('@prefix : <https://oscarvl.synology.me:8443/covid-impacts/schema/> .\n')
         file.write('@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n')
         file.write('@prefix owl: <http://www.w3.org/2002/07/owl#> .\n')
         file.write('@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n')
@@ -61,7 +61,7 @@ class ModelCovidImpacts:
 
 
     def _create_data_header(self, file):
-        file.write('@prefix : <http://oscarvl.synology.me/data/> .\n')
+        file.write('@prefix : <https://oscarvl.synology.me:8443/covid-impacts/data/> .\n')
         file.write('@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n')
         file.write('@prefix dc: <http://purl.org/dc/elements/1.1/> .\n')
         file.write('@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n')
