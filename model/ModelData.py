dbd6786e8ef6a4014d0de1d3db6c9c90a0174378
OscarVanL <7999692+OscarVanL@users.noreply.github.com>
2021-03-18 16:31:19 +0000

    Removed unused namespaces from schema and data

--- model/ModelData.py
+++ model/ModelData.py
@@ -31,9 +31,7 @@ class ModelCovidImpacts:
     def _create_schema(self, file):
         file.write('@prefix : <http://oscarvl.synology.me/schema/covid-impacts/> .\n')
         file.write('@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n')
-        file.write('@prefix dc: <http://purl.org/dc/elements/1.1/> .\n')
         file.write('@prefix owl: <http://www.w3.org/2002/07/owl#> .\n')
-        file.write('@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n')
         file.write('@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n')
         file.write('@prefix qb: <http://purl.org/linked-data/cube#> .\n\n')
 
@@ -67,7 +65,6 @@ class ModelCovidImpacts:
         file.write('@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n')
         file.write('@prefix dc: <http://purl.org/dc/elements/1.1/> .\n')
         file.write('@prefix owl: <http://www.w3.org/2002/07/owl#> .\n')
-        file.write('@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n')
         file.write('@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n')
         file.write('@prefix qb: <http://purl.org/linked-data/cube#> .\n')
         file.write('@prefix covid-impacts: <http://oscarvl.synology.me/schema/covid-impacts/> .\n\n')
