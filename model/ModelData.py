f26979f1d2f5a54c823e875d4c54e9dea40fb818
OscarVanL <7999692+OscarVanL@users.noreply.github.com>
2021-03-18 16:33:44 +0000

    Removed unused owl namespace

--- model/ModelData.py
+++ model/ModelData.py
@@ -64,7 +64,6 @@ class ModelCovidImpacts:
         file.write('@prefix : <http://oscarvl.synology.me/data/> .\n')
         file.write('@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n')
         file.write('@prefix dc: <http://purl.org/dc/elements/1.1/> .\n')
-        file.write('@prefix owl: <http://www.w3.org/2002/07/owl#> .\n')
         file.write('@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n')
         file.write('@prefix qb: <http://purl.org/linked-data/cube#> .\n')
         file.write('@prefix covid-impacts: <http://oscarvl.synology.me/schema/covid-impacts/> .\n\n')
